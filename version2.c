ee608ac1b3a604ce447232dde8eddfc89cd8f091
Vishu Arora <vishu.a011@gmail.com>
2026-06-08 01:00:34 +0530

    Refactor student data input and roll number handling

--- version2.c
+++ version2.c
@@ -13,31 +13,40 @@ int main(){
         printf("The file is opened unsuccessfully.\n");
         return 1;
     }
-    int rollnumber = 1;
+    
 
     while (1){
         char name[100];
-        int age;
+        int age, rollnumber;
         float cgpa;
 
+        //to get the name of the student
         printf("Enter the name: ");
         scanf(" ");
         fgets(name, sizeof(name), stdin);
         name[strcspn(name, "\n")] = '\0';
 
+        //to get the age of the student
         printf("Enter the age: ");
         scanf("%d", &age);
 
+        //to get the cgpa of the student
         printf("Enter the CGPA: ");
         scanf("%f", &cgpa);
 
+        //to get the roll number of the student
+        printf("Enter the Roll Number: ");
+        scanf("%d", &rollnumber);
+
+        //to send the data in file 'student.txt'
         fprintf(fptr, "Name: %s\nAge: %d\nCGPA: %f\nRoll Number: %d\n\n", name, age, cgpa, rollnumber++);
 
 
-        printf("Do you want to add more students: (y/n)");
+        //to get if the user wants to continue entering new data
+        printf("Do you want to add more students(y/n): ");
         scanf(" %c", &choice);
 
-        if(choice == 'n') break;
+        if(choice == 'n' || choice == 'N') break;
     }
 
     fclose(fptr);
